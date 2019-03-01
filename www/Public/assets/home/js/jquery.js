9729163e863db138234c5ce70d12452214daa4ff
1324850311@qq.com <1324850311@qq.com>
2019-03-01 15:31:51 +0800

    20190301_updata_2

--- www/Public/assets/home/js/jquery.js
+++ www/Public/assets/home/js/jquery.js
@@ -1846,12 +1846,3 @@ if(JH.browserandroid()||JH.browserios()||JH.browserwx()){
 	var host = window.location.host;
 	window.location.href = "http://"+host+"/wap/";
 }
-jQuery.event.special.touchstart = {
-  setup: function( _, ns, handle ){
-    if ( ns.includes("noPreventDefault") ) {
-      this.addEventListener("touchstart", handle, { passive: false });
-    } else {
-      this.addEventListener("touchstart", handle, { passive: true });
-    }
-  }
-};
