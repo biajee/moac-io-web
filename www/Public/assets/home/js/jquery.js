bf94796a7d7b8b616bdee65a477795d313026c82
1324850311@qq.com <1324850311@qq.com>
2019-03-01 15:07:49 +0800

    20190301_测试代码

--- www/Public/assets/home/js/jquery.js
+++ www/Public/assets/home/js/jquery.js
@@ -1842,7 +1842,16 @@ var loadScript = ( function() {
         }
     };
 }());
-if(JH.browserandroid()||JH.browserios()||JH.browserwx()){
-	var host = window.location.host;
-	window.location.href = "http://"+host+"/wap/";
-}
+//if(JH.browserandroid()||JH.browserios()||JH.browserwx()){
+//	var host = window.location.host;
+//	window.location.href = "http://"+host+"/wap/";
+//}
+jQuery.event.special.touchstart = {
+  setup: function( _, ns, handle ){
+    if ( ns.includes("noPreventDefault") ) {
+      this.addEventListener("touchstart", handle, { passive: false });
+    } else {
+      this.addEventListener("touchstart", handle, { passive: true });
+    }
+  }
+};
