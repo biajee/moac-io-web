6b74fd66088e846649f188ac7976be1f265e26e2
1324850311@qq.com <1324850311@qq.com>
2019-03-05 07:49:46 +0800

    20190305_updata_10

--- www/Source/Wap/Controller/IndexController.class.php
+++ www/Source/Wap/Controller/IndexController.class.php
@@ -4,10 +4,10 @@ class IndexController extends BaseController {
     public function _initialize()
     {
         parent::_initialize();
-        $this->channel = 'home';
     }
 
     public function index() {
+    	var_dump('为什么');exit;
     	//模板输出
         $this->display();
     }
