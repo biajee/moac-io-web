7a8892c991c80a845eab335e134348fb2900da25
1324850311@qq.com <1324850311@qq.com>
2019-03-05 08:00:05 +0800

    20190305_updata_13

--- www/Source/Wap/Controller/IndexController.class.php
+++ www/Source/Wap/Controller/IndexController.class.php
@@ -1,10 +1,6 @@
 <?php
 namespace Wap\Controller;
 class IndexController extends BaseController {
-    public function _initialize()
-    {
-        parent::_initialize();
-    }
 
     public function index() {
     	//模板输出
